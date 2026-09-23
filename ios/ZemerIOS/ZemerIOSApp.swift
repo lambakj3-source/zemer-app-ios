a3e2ed43bda940a2da69d334101190bce87d76e2
lambakj3-source <lambakj3@gmail.com>
2026-09-23 12:30:38 -0700

    Inject local library store

--- ios/ZemerIOS/ZemerIOSApp.swift
+++ ios/ZemerIOS/ZemerIOSApp.swift
@@ -3,11 +3,13 @@ import SwiftUI
 @main
 struct ZemerIOSApp: App {
     @StateObject private var player = AudioPlayer()
+    @StateObject private var library = LibraryStore()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(player)
+                .environmentObject(library)
                 .tint(.primary)
         }
     }
